23bced662e7935429214cf6a72573e1fc898bc5b
ghiyatsa <ghiyatsa@users.noreply.github.com>
2026-09-17 08:36:48 +0000

    refactor(admin): ringkas statistik kunjungan menjadi tiga kartu
    Kartu "Minggu Ini" dibuang karena rentangnya berbeda dari tiga kartu
    lain yang semuanya menghitung kunjungan hari ini, dan keberadaannya
    menyisakan dua slot kosong pada grid tiga kolom. Ringkasan kunjungan
    kini memuat total hari ini, mahasiswa, serta staf dan umum.

--- app/Filament/Widgets/TodayVisitorsWidget.php
+++ app/Filament/Widgets/TodayVisitorsWidget.php
@@ -19,7 +19,6 @@ class TodayVisitorsWidget extends StatsOverviewWidget
     protected function getStats(): array
     {
         [$todayStart, $todayEnd] = VisitLog::adminDayRange();
-        [$weekStart, $weekEnd] = VisitLog::adminWeekRange();
 
         $total = VisitLog::query()->whereBetween('visited_at', [$todayStart, $todayEnd])->count();
 
@@ -76,22 +75,6 @@ class TodayVisitorsWidget extends StatsOverviewWidget
                 ->icon(Heroicon::OutlinedBriefcase)
                 ->url(VisitLogResource::getUrl('index', ['filters' => ['today' => ['isActive' => true]]])),
 
-            Stat::make('Minggu Ini', VisitLog::query()
-                ->whereBetween('visited_at', [$weekStart, $weekEnd])
-                ->count())
-                ->description('Total kunjungan minggu ini')
-                ->descriptionColor('gray')
-                ->descriptionIcon(Heroicon::OutlinedCalendarDays)
-                ->color('primary')
-                ->icon(Heroicon::OutlinedCalendarDays)
-                ->url(VisitLogResource::getUrl('index', [
-                    'filters' => [
-                        'visited_between' => [
-                            'visited_from' => now()->startOfWeek()->toDateString(),
-                            'visited_until' => now()->endOfWeek()->toDateString(),
-                        ],
-                    ],
-                ])),
         ];
     }
 }
