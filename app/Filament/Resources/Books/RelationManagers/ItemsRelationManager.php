d332043d20465f8476853ede0830221084bd7479
Ghiyatsa <144647194+ghiyatsa@users.noreply.github.com>
2026-09-19 00:50:45 +0700

    refactor(filament): aktifkan strict_types pada resource books

--- app/Filament/Resources/Books/RelationManagers/ItemsRelationManager.php
+++ app/Filament/Resources/Books/RelationManagers/ItemsRelationManager.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Filament\Resources\Books\RelationManagers;
 
 use App\Filament\Resources\Books\RelationManagers\Actions\BatchCreateBookItemsAction;
--- app/Filament/Resources/Books/Schemas/BookForm.php
+++ app/Filament/Resources/Books/Schemas/BookForm.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Filament\Resources\Books\Schemas;
 
 use App\Filament\Resources\Authors\Schemas\AuthorForm;
