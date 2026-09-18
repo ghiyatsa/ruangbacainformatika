90e3c11b95deeaa1e79d139b5f76b8ca004a83ec
Ghiyatsa <144647194+ghiyatsa@users.noreply.github.com>
2026-09-19 00:51:13 +0700

    refactor(filament): aktifkan strict_types pada resource publishers

--- app/Filament/Resources/Publishers/RelationManagers/BooksRelationManager.php
+++ app/Filament/Resources/Publishers/RelationManagers/BooksRelationManager.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Filament\Resources\Publishers\RelationManagers;
 
 use App\Filament\Resources\Concerns\BookRelationManager;
--- app/Filament/Resources/Publishers/Schemas/PublisherForm.php
+++ app/Filament/Resources/Publishers/Schemas/PublisherForm.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Filament\Resources\Publishers\Schemas;
 
 use App\Models\Publisher;
